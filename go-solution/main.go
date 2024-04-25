72058149bd13bd81d1363afcddbc3e019496ab89
Sayan Mallick <snyxmk@gmail.com>
2024-04-25 20:54:46 +0530

    go 7.7s

--- go-solution/main.go
+++ go-solution/main.go
@@ -3,82 +3,266 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"math"
 	"os"
-	"slices"
+	"runtime"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
+	"unsafe"
 )
 
-const measurementsPath = "../measurements.txt"
+const (
+	deafultMeasurementsPath = "../measurements.txt"
+	maxNameLen              = 100
+	maxNameNum              = 10000
 
-type Measurement struct {
-	Min   float64
-	Max   float64
-	Sum   float64
-	Count int
+	// TODO tune for M1 po
+	defaultParseChunkSizeMB = 32
+	mb                      = 1024 * 1024
+)
+
+type Stats struct {
+	Min, Max, Sum float64
+	Count         int
 }
 
-func main() {
-	dataFile, err := os.Open(measurementsPath)
-	if err != nil {
-		panic(err)
+// Rounding floats to 1 decimal place with 0.05 rounding up to 0.1
+func round(x float64) float64 {
+	return math.Floor((x+0.05)*10) / 10
+}
+
+// parseFloatFast is a high performance float parser
+// using the assumption that the byte silce will
+// alway have a single decimal digit
+func parseFloatFast(bs []byte) float64 {
+	var intStartIdx int // is negative
+	if bs[0] == '-' {
+		intStartIdx = 1
 	}
 
-	defer dataFile.Close()
+	v := float64(bs[len(bs)-1]-'0') / 10
+	place := 1.0
+	for i := len(bs) - 3; i >= intStartIdx; i-- {
+		v += float64(bs[i]-'0') * place
+		place *= 10
+	}
 
-	measurements := make(map[string]*Measurement)
+	if intStartIdx == 1 {
+		v *= -1
+	}
 
-	fileScanner := bufio.NewScanner(dataFile)
-	fileScanner.Split(bufio.ScanLines)
+	return v
+}
 
-	for fileScanner.Scan() {
-		rawString := fileScanner.Text()
-		stationName, termperatureStr, found := strings.Cut(rawString, ";")
-		if !found {
-			continue
+// size is the intended number of bytes to parse. buffer should be longer than size
+// because we need to continue reading until the end of the line in order to
+// properly segment the entire file and not miss any data.
+func parseAt(f *os.File, buf []byte, offset int64, size int) map[string]*Stats {
+	stats := make(map[string]*Stats, maxNameNum)
+	n, err := f.ReadAt(buf, offset) // load the buffer
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+
+	lastName := make([]byte, maxNameLen) // last name parsed
+	var lastNameLen int
+	isScanningName := true // currently scanning name or value?
+
+	// if offset is non-zero, skip to the first new line
+	var idx, start int
+	if offset != 0 {
+		for idx < n {
+			if buf[idx] == '\n' {
+				idx++
+				start = idx
+				break
+			}
+			idx++
 		}
+	}
+	// tick tock between parsing names and values while accummulating stats
+	for {
+		if isScanningName {
+			for idx < n {
+				if buf[idx] == ';' {
+					nameBs := buf[start:idx]
+					lastNameLen = copy(lastName, nameBs)
 
-		termperature, err := strconv.ParseFloat(termperatureStr, 32)
-		if err != nil {
-			panic(err)
-		}
-
-		measurement := measurements[stationName]
-		if measurement == nil {
-			measurements[stationName] = &Measurement{
-				Min:   termperature,
-				Max:   termperature,
-				Sum:   termperature,
-				Count: 1,
+					idx++
+					start = idx
+					isScanningName = false
+					break
+				}
+				idx++
 			}
 		} else {
-			measurement.Max = max(measurement.Max, termperature)
-			measurement.Min = min(measurement.Min, termperature)
-			measurement.Sum += termperature
-			measurement.Count += 1
+			for idx < n {
+				if buf[idx] == '\n' {
+					valueBs := buf[start:idx]
+					value := parseFloatFast(valueBs)
+
+					nameUnsafe := unsafe.String(&lastName[0], lastNameLen)
+					if s, ok := stats[nameUnsafe]; !ok {
+						name := string(lastName[:lastNameLen]) // actually allocate string
+						stats[name] = &Stats{Min: value, Max: value, Sum: value, Count: 1}
+					} else {
+						if value < s.Min {
+							s.Min = value
+						}
+						if value > s.Max {
+							s.Max = value
+						}
+						s.Sum += value
+						s.Count++
+					}
+
+					idx++
+					start = idx
+					isScanningName = true
+					break
+				}
+				idx++
+			}
+		}
+		// terminate when we hit the first newline after the intended size OR
+		// when we hit the end of the file
+		if (isScanningName && idx >= size) || idx >= n {
+			break
 		}
 	}
 
-	printResults(measurements)
+	return stats
 }
 
-func printResults(results map[string]*Measurement) {
-	// sort by station name
-	stationNames := make([]string, 0, len(results))
-	for stationName := range results {
-		stationNames = append(stationNames, stationName)
+func printResults(stats map[string]*Stats) { // doesn't help
+	// sorted alphabetically for output
+	names := make([]string, 0, len(stats))
+	for name := range stats {
+		names = append(names, name)
 	}
+	sort.Strings(names)
 
-	slices.Sort(stationNames)
-
-	fmt.Print("{")
-	for idx, stationName := range stationNames {
-		measurement := results[stationName]
-		mean := measurement.Sum / float64(measurement.Count)
-		fmt.Printf("%s=%.1f/%.1f/%.1f", stationName, measurement.Min, mean, measurement.Max)
-		if idx < len(stationNames)-1 {
-			fmt.Print(", ")
+	var builder strings.Builder
+	for i, name := range names {
+		s := stats[name]
+		// gotcha: first round the sum to to remove float precision errors!
+		avg := round(round(s.Sum) / float64(s.Count))
+		builder.WriteString(fmt.Sprintf("%s=%.1f/%.1f/%.1f", name, s.Min, avg, s.Max))
+		if i < len(names)-1 {
+			builder.WriteString(", ")
 		}
 	}
-	fmt.Println("}")
+
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fprintf(writer, "{%s}\n", builder.String())
+	writer.Flush()
+}
+
+// Read file in chunks and parse concurrently. N parsers work off of a chunk
+// offset chan and send results on an output chan. The results are merged into a
+// single map of stats and printed.
+
+func main() {
+	var err error
+
+	var numParsers int
+	{
+		if os.Getenv("NUM_PARSERS") != "" {
+			numParsers, err = strconv.Atoi(os.Getenv("NUM_PARSERS"))
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to parse NUM_PARSERS: %w", err))
+			}
+		} else {
+			numParsers = runtime.NumCPU()
+		}
+	}
+
+	var parseChunkSize int
+	{
+		if os.Getenv("PARSE_CHUNK_SIZE_MB") != "" {
+			parseChunkSizeMB, err := strconv.Atoi(os.Getenv("PARSE_CHUNK_SIZE_MB"))
+			if err != nil {
+				log.Fatal(fmt.Errorf("failed to parse PARSE_CHUNK_SIZE_MB: %w", err))
+			}
+			parseChunkSize = parseChunkSizeMB * mb
+		} else {
+			parseChunkSize = defaultParseChunkSizeMB * mb
+		}
+	}
+
+	measurementsPath := deafultMeasurementsPath
+	if len(os.Args) > 1 {
+		measurementsPath = os.Args[1]
+	}
+
+	// read file
+	f, err := os.Open(measurementsPath)
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to open %s file: %w", measurementsPath, err))
+	}
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to read %s file: %w", measurementsPath, err))
+	}
+
+	// kick off "parser" workers
+	wg := sync.WaitGroup{}
+	wg.Add(numParsers)
+
+	// buffered to not block on merging
+	chunkOffsetCh := make(chan int64, numParsers)
+	chunkStatsCh := make(chan map[string]*Stats, numParsers)
+
+	go func() {
+		i := 0
+		for i < int(info.Size()) {
+			chunkOffsetCh <- int64(i)
+			i += parseChunkSize
+		}
+		close(chunkOffsetCh)
+	}()
+
+	for i := 0; i < numParsers; i++ {
+		// WARN: w/ extra padding for line overflow. Each chunk should be read past
+		// the intended size to the next new line. 128 bytes should be enough for
+		// a max 100 byte name + the float value.
+		buf := make([]byte, parseChunkSize+128)
+		go func() {
+			for chunkOffset := range chunkOffsetCh {
+				chunkStatsCh <- parseAt(f, buf, chunkOffset, parseChunkSize)
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(chunkStatsCh)
+	}()
+
+	mergedStats := make(map[string]*Stats, maxNameNum)
+	for chunkStats := range chunkStatsCh {
+		for name, s := range chunkStats {
+			if ms, ok := mergedStats[name]; !ok {
+				mergedStats[name] = s
+			} else {
+				if s.Min < ms.Min {
+					ms.Min = s.Min
+				}
+				if s.Max > ms.Max {
+					ms.Max = s.Max
+				}
+				ms.Sum += s.Sum
+				ms.Count += s.Count
+			}
+		}
+	}
+
+	printResults(mergedStats)
 }
